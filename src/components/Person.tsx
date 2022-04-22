bbd86b88905e637fae013201888fb10d25cebdd9
icecrimu <charles.banigon@gmail.com>
2022-04-22 11:16:22 +0800

    useState in typescript

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+import { ChangeEvent, FC, useState } from "react"
 
 interface IPersonProps {
   email: string
@@ -7,11 +7,20 @@ interface IPersonProps {
 }
 
 export const Person: FC<IPersonProps> = ({ email, name, age }) => {
+  const [country, setCountry] = useState<string | null>("")
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCountry(e.target.value)
+  }
+
   return (
     <div>
       <h1>{email}</h1>
       <h1>{name}</h1>
       <h1>Age:{age}</h1>
+
+      <input placeholder="Write down your country..." onChange={handleChange} />
+      {country}
     </div>
   )
 }
