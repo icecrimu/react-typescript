9af1397c41ab908b0529968fe84b6a4c3122b0f9
icecrimu <charles.banigon@gmail.com>
2022-04-22 13:57:47 +0800

    Context API typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import React, { FC } from "react"
+import React, { FC, createContext } from "react"
 import "./App.css"
 import { Person, HairColor } from "./components/Person"
 
+interface AppContextInterface {
+  name: string
+  age: number
+  country: string
+}
+
+const AppContext = createContext<AppContextInterface | null>(null)
+
 const App: FC = () => {
+  const contextValue: AppContextInterface = {
+    name: "Charles",
+    age: 22,
+    country: "Philippines"
+  }
+
   return (
-    <div className="App">
-      <Person
-        email="sample@gmail.com"
-        name="Charles"
-        hairColor={HairColor.Pink}
-      />
-    </div>
+    <AppContext.Provider value={contextValue}>
+      <div className="App">
+        <Person
+          email="sample@gmail.com"
+          name="Charles"
+          hairColor={HairColor.Pink}
+        />
+      </div>
+    </AppContext.Provider>
   )
 }
 
